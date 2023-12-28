e8728a5e6814d49d4c2f0f4723569806573252a7
Kushagra <you@example.com>
2023-12-27 19:07:48 -0500

    Lotsa changes. Made a script which automates taking the first arg of image and assingng it to the rest of the func. (all that becuase turns out image doesnt allow attrs)

--- expirements/FAST.py
+++ expirements/FAST.py
@@ -1,17 +1,27 @@
 import ast
+import astunparse
 
-with open(filePath, 'f') as file:
-    text = file.readlines()
+
 
 tree = ast.parse(text)
-
-class functionCallEachTime(ast.NodeVisitor):
+argumentIndexNumber = 0
+class ChangeImageFunctionCall(ast.NodeTransformer):
     def callFunctionForEachFunctionCallinTree(self, node):
         if 42 <= node.lineo <= 62:
-            if node.args:
-                firstArgument = node.args[0]
-                if isinstance(firstArgument, ast.Name):
-                    node.args[0] = ast.Str(firstArgument.id)
-        self.generic_visit(node)
-call = functionCallEachTime()
-call.visit(tree)
+            if isinstance(node.func, ast.Name) and node.func.id == 'image':
+                # just for readiblity argumentIndexNumber = 0
+                variableName = node.args[argumentIndexNumber].s
+                secondArgument = node.args[argumentIndexNumber + 1]
+                thirdArgument = node.args[argumentIndexNumber + 2]
+                newFunctionCall = ast.parse(f'{variableName} = image({secondArgument}, {thirdArgument})')
+                return newFunctionCall
+            return node
+
+
+with open('Code/first-game.py', 'r') as file:
+    tree = ast.parse(file.read())
+
+tree = ChangeImageFunctionCall().visit(tree)
+
+with open('Code/first-game.py', 'w') as file:
+    file.write(astunparse.unparse(tree))
