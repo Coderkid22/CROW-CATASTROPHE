2eeae30bc955b42c1a0a1ee75d4961055539ffff
Kushagra <you@example.com>
2023-12-27 20:46:26 -0500

    eh it was funny tho lol HAKERMAN

--- expirements/FAST.py
+++ expirements/FAST.py
@@ -2,7 +2,7 @@ import ast
 import astunparse
 import shutil
 
-shutil.copy('code/first-game.py', 'first-game.py.bak')
+shutil.copy('source path', 'copy path')
 
 with open('Code/first-game.py', 'r') as file:
     code = file.read()
--- expirements/random-stuff.py
+++
@@ -1,3 +0,0 @@
-sqlcode = input('pls don do sql injection Hakerman ')
-sql = sqlcode
-exec(sqlcode)
